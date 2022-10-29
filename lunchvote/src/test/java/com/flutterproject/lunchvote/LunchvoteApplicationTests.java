b2483ae7282adc831de0c8eeaa8a2b3fc3e2fd37
jeonghoon <shb03323@naver.com>
2022-10-30 00:03:10 +0900

    feat: 프로젝트 초기 세팅

---
+++ lunchvote/src/test/java/com/flutterproject/lunchvote/LunchvoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flutterproject.lunchvote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LunchvoteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
