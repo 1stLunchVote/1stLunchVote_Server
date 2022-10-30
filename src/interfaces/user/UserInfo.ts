af8ec3fdcecf55af20275a33697cdb4d23c70cee
jeonghoon <shb03323@naver.com>
2022-10-30 21:53:30 +0900

    feat: User Schema 생성

---
+++ src/interfaces/user/UserInfo.ts
@@ -0,0 +1,6 @@
+export interface UserInfo {
+  socialType: string;
+  socialId: string;
+  email: string;
+  nickname: string;
+}
---
+++ src/models/User.ts
@@ -0,0 +1,26 @@
+import mongoose from 'mongoose';
+import { UserInfo } from '../interfaces/user/UserInfo';
+
+const UserSchema = new mongoose.Schema(
+  {
+    socialType: {
+      type: String,
+      enum: ['KAKAO'],
+      required: true,
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+    },
+    nickname: {
+      type: String,
+      require: true,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+export default mongoose.model<UserInfo & mongoose.Document>('User', UserSchema);
