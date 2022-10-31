b5e4b571e8ebf41dba6b43d5942adf37b5fe8c9c
jeonghoon <shb03323@naver.com>
2022-10-31 16:18:44 +0900

    feat: 닉네임 변경 api 라우터 설정

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import AuthRouter from './AuthRouter';
+import UserRouter from './UserRouter';
 
 const router: Router = Router();
 
 router.use('/auth', AuthRouter);
+router.use('/user', UserRouter);
 
 export default router;
