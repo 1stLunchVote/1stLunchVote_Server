07bb4e3a2ad8a6de2970c7da5b91707be8e40d09
jeonghoon <shb03323@naver.com>
2022-11-03 15:04:56 +0900

    feat: 그룹 이름 변경 라우터 연결

--- src/routes/GroupRouter.ts
+++ src/routes/GroupRouter.ts
@@ -9,5 +9,6 @@ router.get('/', auth, GroupContoller.getAllGroup);
 router.get('/:groupId', auth, GroupContoller.getGroup);
 router.patch('/:groupId/invite', auth, GroupContoller.inviteMember);
 router.patch('/:groupId/exile', auth, GroupContoller.exileMember);
+router.patch('/:groupId/groupName', auth, GroupContoller.updateGroupName);
 
 export default router;
