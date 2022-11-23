e05a661a410a68961c466a365c62910525a87460
jeonghoon <shb03323@naver.com>
2022-11-23 15:51:43 +0900

    feat: GroupService에서 inviteMember 1차 생성

--- src/services/GroupService.ts
+++ src/services/GroupService.ts
@@ -35,8 +35,49 @@ const postGroup = async (userId: string): Promise<PostGroupResponseDto | string>
   }
 };
 
+const inviteMember = async (groupId: string, email: string): Promise<PostGroupResponseDto | string> => {
+  try {
+    const member = await User.findOne({ email: email });
+    if (!member) {
+      return responseMessage.NO_USER;
+    }
+
+    const group = await Group.findById(groupId).populate('members');
+    if (!group) {
+      return responseMessage.NO_GROUP;
+    }
+    console.log(group);
+
+    group.members.push(member._id);
+    group.update();
+    console.log(group);
+
+    // const members = Promise.all(
+    //   group.members.map(async (member) => {
+    //     const result: UserInfo = {
+    //       email: member.email,
+    //       nickname: member.nickname
+    //     };
+
+    //     return result;
+    //   })
+    // )
+
+    // const data: PostGroupResponseDto = {
+    //   groupId: group._id,
+    //   members: members,
+    // };
+
+    return '';
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
+
 const GroupService = {
   postGroup,
+  inviteMember,
 };
 
 export default GroupService;
