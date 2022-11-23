3b280d317974708578834b8e8f887fefd1d46e73
jeonghoon <shb03323@naver.com>
2022-11-23 15:56:09 +0900

    feat: GroupController에서 inviteMember 생성

--- src/controllers/GroupController.ts
+++ src/controllers/GroupController.ts
@@ -24,8 +24,30 @@ const postGroup = async (req: Request, res: Response) => {
   }
 };
 
+/**
+ *  @route Patch /:groupId/invite
+ *  @desc patch group members
+ *  @access Public
+ */
+const inviteMember = async (req: Request, res: Response) => {
+  try {
+    const groupId = req.params.groupId;
+    const email = req.body.email;
+    const data = await GroupService.inviteMember(groupId, email);
+    if (data === message.NO_USER) {
+      return res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, message.NO_USER));
+    }
+
+    res.status(statusCode.CREATED).send(util.success(statusCode.CREATED, message.INVITE_MEMBER_SUCCESS, data));
+  } catch (error) {
+    console.log(error);
+    res.status(statusCode.INTERNAL_SERVER_ERROR).send(util.fail(statusCode.INTERNAL_SERVER_ERROR, message.INTERNAL_SERVER_ERROR));
+  }
+};
+
 const GroupContoller = {
   postGroup,
+  inviteMember,
 };
 
 export default GroupContoller;
