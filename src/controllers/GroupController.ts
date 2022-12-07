695fccbb47dab0ec98618b6fd11647eebf548859
jeonghoon <shb03323@naver.com>
2022-12-07 16:08:44 +0900

    [Feat] 2차 투표 상태 조회 수정

--- src/controllers/GroupController.ts
+++ src/controllers/GroupController.ts
@@ -186,7 +186,7 @@ const secondVote = async (req: Request, res: Response) => {
 const getSecondVoteStatus = async (req: Request, res: Response) => {
   try {
     const groupId = req.params.groupId;
-    const data = await GroupService.getFirstVoteStatus(groupId);
+    const data = await GroupService.getSecondVoteStatus(groupId);
     if (data === message.NO_GROUP) {
       return res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, message.NO_GROUP));
     }
